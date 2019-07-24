fd091207c42c101f86a4e8909c8398808179a7cb
ManjunathaKaliwal <manjunatha.kaliwal@finastra.com>
2019-07-24 12:06:48 +0530

    Initial Project Setup

---
+++ FWDemo/Calculation.swift
@@ -0,0 +1,19 @@
+//
+//  Calculation.swift
+//  FWDemo
+//
+//  Created by Manjunatha kaliwal on 24/07/19.
+//  Copyright © 2019 Manjunatha kaliwal. All rights reserved.
+//
+
+import Foundation
+
+let name = "FWDemo"
+
+public func add(a: Int, b: Int) -> Int {
+    return a + b
+}
+
+public func sub(a: Int, b: Int) -> Int {
+    return a - b
+}
---
+++ FWDemo/FWDemo.h
@@ -0,0 +1,19 @@
+//
+//  FWDemo.h
+//  FWDemo
+//
+//  Created by Manjunatha kaliwal on 24/07/19.
+//  Copyright © 2019 Manjunatha kaliwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FWDemo.
+FOUNDATION_EXPORT double FWDemoVersionNumber;
+
+//! Project version string for FWDemo.
+FOUNDATION_EXPORT const unsigned char FWDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FWDemo/PublicHeader.h>
+
+
